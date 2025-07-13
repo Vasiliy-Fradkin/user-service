816d4c351476c7306d2394259fbcd32bb3a4a0a4
Vasiliy-Fradkin <vasiliy9774@gmail.com>
2025-07-13 14:00:14 +0500

    using Service in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,16 @@
 package org.example;
 
-import org.example.dao.UserDao;
-import org.example.dao.UserDaoImpl;
+
 import org.example.entity.User;
+import org.example.service.UserService;
+import org.example.service.UserServiceImpl;
 import org.example.util.HibernateUtil;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoImpl();
+        UserService userService = new UserServiceImpl();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -35,25 +36,25 @@ public class Main {
                     System.out.print("Возраст: ");
                     user.setAge(Integer.parseInt(scanner.nextLine()));
 
-                    userDao.createUser(user);
+                    userService.createUser(user);
                     System.out.println("Пользователь создан!");
                 }
                 case "2" -> {
                     System.out.print("Введите ID: ");
                     int id = Integer.parseInt(scanner.nextLine());
-                    userDao.getUserById(id)
+                    userService.getUserById(id)
                             .ifPresentOrElse(
                                     System.out::println,
                                     () -> System.out.println("Пользователь не найден")
                             );
                 }
                 case "3" -> {
-                    userDao.getAllUsers().forEach(System.out::println);
+                    userService.getAllUsers().forEach(System.out::println);
                 }
                 case "4" -> {
                     System.out.print("Введите ID пользователя для обновления: ");
                     int id = Integer.parseInt(scanner.nextLine());
-                    var optUser = userDao.getUserById(id);
+                    var optUser = userService.getUserById(id);
                     if (optUser.isPresent()) {
                         User user = optUser.get();
                         System.out.print("Новое имя (" + user.getName() + "): ");
@@ -62,7 +63,7 @@ public class Main {
                         user.setEmail(scanner.nextLine());
                         System.out.print("Новый возраст (" + user.getAge() + "): ");
                         user.setAge(Integer.parseInt(scanner.nextLine()));
-                        userDao.updateUser(user);
+                        userService.updateUser(user);
                         System.out.println("Пользователь обновлён");
                     } else {
                         System.out.println("Пользователь не найден");
@@ -71,7 +72,7 @@ public class Main {
                 case "5" -> {
                     System.out.print("Введите ID для удаления: ");
                     int id = Integer.parseInt(scanner.nextLine());
-                    userDao.deleteUser(id);
+                    userService.deleteUser(id);
                     System.out.println("Пользователь удалён");
                 }
                 case "0" -> {
